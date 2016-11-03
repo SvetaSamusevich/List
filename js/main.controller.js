c22627cd17dd7733ee3202f9b4a73a9769a6bf2d
SvetaSamusevich <samusevich2@yandex.by>
2016-11-03 18:07:02 +0300

    Add files via upload

---
+++ js/main.controller.js
@@ -0,0 +1,18 @@
+﻿(function(){
+
+	angular
+	 .module('App')	
+	 .controller('MainController', MainController);
+	
+	MainController.$inject = ['listService'];
+	
+	function MainController(listService){
+	 var vm = this;
+	 
+	 vm.load =  listService.setObject();	
+	 vm.object = listService.getObject();
+	 
+	 
+	
+	}
+})();
---
+++ js/service/list.service.js
@@ -0,0 +1,50 @@
+﻿(function(){
+	angular
+		.module('App.service')
+		.service('listService',ListService);
+		
+	ListService.$inject = ['$http','$q'];
+	
+	function ListService($http,$q){
+		var ser = this;
+		
+		//
+		var object = {};
+		
+		object.firstList=[];
+		object.secondList=[];
+		object.thirdList=[];
+		
+		ser.getObject = getObject;
+		ser.setObject = setObject;
+		
+		function getObject(){
+			return object;
+		}
+		
+		function random(){
+			var ran = Math.floor(Math.random()*3)+1;
+			return ran;
+		}
+		
+		function setObject(){
+			
+			$http.get("js/service/roster/json/"+random()+".json")
+				.then(function(res){
+						object.firstList = res.data.List;
+						return $http.get(res.data.Road);
+						}
+				)
+				.then(function(res){
+					object.secondList = res.data.List;
+						return $http.get(res.data.Road);
+					}
+				)
+				.then(function(res){
+					object.thirdList = res.data.List;
+					return $http.get(res.data.Road);
+					}
+				)
+		}
+	}
+})();
